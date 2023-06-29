d833146c2a04daee9046777d110d15cbe102dd38
hyojinx <oozxoocv82@gmail.com>
2023-06-29 12:34:14 +0900

    pythonstudy commit

---
+++ 06Function.py
@@ -0,0 +1,69 @@
+#함수
+def print_max(a,b):
+    if a>b:
+        print(a,'is bigger than',b)
+    elif a==b:
+        print(a,'is equal to',b)
+    else:
+        print(b,'is bigger than',a)
+print_max(5,7)
+
+def mul(a,b=1):
+    c=a*b
+    return(c)
+print(mul(3,7))
+print(mul(2))
+
+#전역 및 지역변수
+x=100
+def func(x):
+    print(x)
+    x=2
+    print(x)
+func(x)
+
+def func2():
+    global x
+    print(x)
+    x=2
+    print(x)
+func2()
+
+def f(i,lst):
+    i+=1
+    print(i)
+    lst.append(0)
+k=100
+m=[1,2,3]
+f(k,m)
+print(k,m)
+
+def reprint(a,b,c):
+    print(a+2,b*2,c/2)
+reprint(6,4,16)
+reprint(c=16,a=6,b=4)
+
+def sum(*numbers):
+    tot=0
+    for num in numbers:
+        tot +=num
+    return tot
+
+print(sum(1,2,3,4,5,6,7,8,9,10))
+
+def sum2(*numbers):
+    count=0
+    tot=0
+    for num in numbers:
+        count+=1
+        tot+=num
+    return count,tot
+
+count,sum=sum2(1,3,4,5,6,7,45,3)
+print(count,sum)
+
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "home", lname = "run")
---
+++ 14Class_object.py
@@ -0,0 +1,38 @@
+class Car:
+    #속성
+    engineCc=0
+    door=0
+    carType=None
+    
+    #생성자
+    def __init__(self,engineCc,door,carType):
+        #멤버변수 초기화
+        self.engineCc=engineCc
+        self.door=door
+        self.carType=carType
+    
+    #Method
+    def display(self):
+        print("자동차는 %d cc이고, 문은 %d개, 타입은 %s"
+              %(self.engineCc,self.door,self.carType))
+
+
+car1 = Car(3000,4,'suv')
+car2 = Car(5000,2,'truck')
+car1.door=10
+car1.display()
+car2.display()
+del car2
+
+import datetime
+from datetime import date,time
+
+today=date(2023,5,5)
+print(today, today.year, today.month, today.day)
+w = today.weekday() # 요일정보 0~6
+print(w)
+
+currTime = time(21,4,30)
+print(currTime, currTime.hour, currTime.minute, currTime.second)
+print(currTime.isoformat())
+
